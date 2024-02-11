47fb4582534b897259eb10a3ca6c31c020e5c682
lage356 <fernando.lage356@gmail.com>
2024-02-10 18:13:14 -0800

    update

--- assets/javascrtip/main.js
+++ assets/javascrtip/main.js
@@ -1,98 +1,54 @@
 var exchangeContainer = document.querySelector(".exchangeData");
-var fromCu = document.getElementById('fromCu');
-var exchangeData = document.getElementById('amountToExchange');
+var fromCu = document.getElementById("fromCu");
+var exchangeData = document.getElementById("amountToExchange");
 var exchangeButton = document.querySelector(".exchangeBtn");
 var exData = exchangeData.value;
 
+var getUSDval = function (input) {
+  var getBaseUsd =
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json";
 
-var getExchangeRate = function () {
-    var currenciesApiUrl =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.min.json";
+  var receivedValue = input;
 
-
-  fetch(currenciesApiUrl)
+  fetch(getBaseUsd)
     .then(function (response) {
-      return response.json();  
+      return response.json();
     })
-    .then(function (data) { 
-      console.log(data)
+    .then(function (data) {
+      let tipoCambio = data.usd.mxn;
+      let dolarToPesos = receivedValue / tipoCambio;
+      let pesosToDolar = tipoCambio * receivedValue;
 
-        for (i in data ){
-      
-            const option1  = new Option (data[i],i)
-            if(data[i] === "US Dollar"){
-              fromCu.add(option1)
-            }else if (data[i]== "Mexican Peso"){
-              fromCu.add(option1)
-            }
-            
-          
-        }
+      renderXchangePesos(pesosToDolar);
 
+      renderXchange(dolarToPesos);
     })
     .catch(function (error) {
       console.log(error);
     });
 };
 
-var getUSDval = function (input) {
-  var getBaseUsd = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json"
-
-  var receivedValue = input ;
-
-fetch(getBaseUsd)
-  .then(function (response) {
-    return response.json();  
-  })
-  .then(function (data) { 
-    
-    let tipoCambio = data.usd.mxn
-    let dolarToPesos = receivedValue / tipoCambio; 
-    let pesosToDolar = tipoCambio * receivedValue; 
-
-    renderXchangePesos(pesosToDolar);
-
-    renderXchange(dolarToPesos);
-
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-};
-
-
-
 var renderXchange = function (data) {
+  var displayDOM = document.getElementById("convertedValue");
 
-  var displayDOM = document.getElementById('convertedValue');
- 
-
-  var valueToDisplay =data ;
+  var valueToDisplay = data;
 
   displayDOM.textContent = valueToDisplay;
- 
+};
 
-}
-
-var renderXchangePesos = function(data){
-  var displayPesos = document.getElementById('convertedValue2')
+var renderXchangePesos = function (data) {
+  var displayPesos = document.getElementById("convertedValue2");
   var valueToPesos = data;
 
-    displayPesos.textContent = valueToPesos;
+  displayPesos.textContent = valueToPesos;
+};
 
-}
+exchangeButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  exData = exchangeData.value;
 
-exchangeButton.addEventListener("click", function(event){
-event.preventDefault();
-exData = exchangeData.value;
-
-var dataToConvert =parseFloat(exData);
-// console.log(dataToConvert)
-// getExchangeRate();
-getUSDval(dataToConvert);
-
-
+  var dataToConvert = parseFloat(exData);
+  // console.log(dataToConvert)
+  // getExchangeRate();
+  getUSDval(dataToConvert);
 });
-
-
-
